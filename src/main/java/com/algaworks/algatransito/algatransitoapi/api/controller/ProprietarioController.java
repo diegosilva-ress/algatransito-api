4b090254f7155f747681e4eab69bc43221c8bc7f
Diego <8083496+diegosilva-ress@users.noreply.github.com>
2025-10-14 20:44:49 -0300

    Criando novo endpoint

--- src/main/java/com/algaworks/algatransito/algatransitoapi/api/controller/ProprietarioController.java
+++ src/main/java/com/algaworks/algatransito/algatransitoapi/api/controller/ProprietarioController.java
@@ -3,20 +3,30 @@ package com.algaworks.algatransito.algatransitoapi.api.controller;
 import com.algaworks.algatransito.algatransitoapi.domain.model.Proprietario;
 import com.algaworks.algatransito.algatransitoapi.domain.repository.ProprietarioRepository;
 import java.util.List;
+import java.util.Optional;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("proprietarios")
 @AllArgsConstructor
 public class ProprietarioController {
 
   private final ProprietarioRepository proprietarioRepository;
 
-  @GetMapping("/proprietarios")
   public List<Proprietario> listar() {
-//    return proprietarioRepository.findAll();
-    return proprietarioRepository.findByNome("Diego");
+    return proprietarioRepository.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Proprietario> buscarPorId(@PathVariable Long id) {
+    return proprietarioRepository.findById(id).map(ResponseEntity::ok)
+        .orElseGet(() -> ResponseEntity.notFound().build());
   }
 
 }
