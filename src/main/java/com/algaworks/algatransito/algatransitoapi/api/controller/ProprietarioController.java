51c53fe9740f29419931cefd2eef6b80f6b2e8d1
Diego <8083496+diegosilva-ress@users.noreply.github.com>
2025-10-14 21:15:28 -0300

    Criando novos endpoints

--- src/main/java/com/algaworks/algatransito/algatransitoapi/api/controller/ProprietarioController.java
+++ src/main/java/com/algaworks/algatransito/algatransitoapi/api/controller/ProprietarioController.java
@@ -6,10 +6,16 @@ import java.util.List;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,6 +25,7 @@ public class ProprietarioController {
 
   private final ProprietarioRepository proprietarioRepository;
 
+  @GetMapping
   public List<Proprietario> listar() {
     return proprietarioRepository.findAll();
   }
@@ -27,6 +34,33 @@ public class ProprietarioController {
   public ResponseEntity<Proprietario> buscarPorId(@PathVariable Long id) {
     return proprietarioRepository.findById(id).map(ResponseEntity::ok)
         .orElseGet(() -> ResponseEntity.notFound().build());
+  }
+
+  @PostMapping
+  @ResponseStatus(code = HttpStatus.CREATED)
+  public Proprietario adicionar(@RequestBody Proprietario proprietario) {
+    return proprietarioRepository.save(proprietario);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Proprietario> atualizar(@PathVariable Long id,
+      @RequestBody Proprietario proprietario) {
+
+    if (proprietarioRepository.existsById(id)) {
+      proprietario.setId(id);
+      return ResponseEntity.ok(proprietarioRepository.save(proprietario));
+    } else {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> remover(@PathVariable Long id) {
+    if (proprietarioRepository.existsById(id)) {
+      proprietarioRepository.deleteById(id);
+      return ResponseEntity.noContent().build();
+    }
+    return ResponseEntity.notFound().build();
   }
 
 }
