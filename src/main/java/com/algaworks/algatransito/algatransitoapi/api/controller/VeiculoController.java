12a0e92765a09ca31db15a47e1687e2708673ed6
Diego <8083496+diegosilva-ress@users.noreply.github.com>
2025-10-24 14:23:32 -0300

    Implementando ModelMapper

--- src/main/java/com/algaworks/algatransito/algatransitoapi/api/controller/VeiculoController.java
+++ src/main/java/com/algaworks/algatransito/algatransitoapi/api/controller/VeiculoController.java
@@ -1,11 +1,13 @@
 package com.algaworks.algatransito.algatransitoapi.api.controller;
 
+import com.algaworks.algatransito.algatransitoapi.api.model.VeiculoModel;
 import com.algaworks.algatransito.algatransitoapi.domain.model.Veiculo;
 import com.algaworks.algatransito.algatransitoapi.domain.repository.VeiculoRepository;
 import com.algaworks.algatransito.algatransitoapi.domain.service.RegistroVeiculoService;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +25,7 @@ public class VeiculoController {
 
   private final VeiculoRepository veiculoRepository;
   private final RegistroVeiculoService registroVeiculoService;
+  private final ModelMapper modelMapper;
 
   @GetMapping
   public List<Veiculo> listar() {
@@ -30,8 +33,9 @@ public class VeiculoController {
   }
 
   @GetMapping("/{veiculoId}")
-  public ResponseEntity<Veiculo> buscar(@PathVariable Long veiculoId) {
+  public ResponseEntity<VeiculoModel> buscar(@PathVariable Long veiculoId) {
     return veiculoRepository.findById(veiculoId)
+        .map(veiculo -> modelMapper.map(veiculo, VeiculoModel.class))
         .map(ResponseEntity::ok)
         .orElse(ResponseEntity.notFound().build());
   }
