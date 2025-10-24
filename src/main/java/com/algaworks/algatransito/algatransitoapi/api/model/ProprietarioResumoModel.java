5336b35118aabb2dff81b5cb9bc17fb8b9745b7c
Diego <8083496+diegosilva-ress@users.noreply.github.com>
2025-10-24 14:48:12 -0300

    Implementando ModelMapper

---
+++ src/main/java/com/algaworks/algatransito/algatransitoapi/api/model/ProprietarioResumoModel.java
@@ -0,0 +1,13 @@
+package com.algaworks.algatransito.algatransitoapi.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProprietarioResumoModel {
+
+  private Long id;
+  private String nome;
+
+}
